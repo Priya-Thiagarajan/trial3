e55445a887f40daa93d0bb5e2683f675b62d6666
Priya Thiagarajan <88799615+Priya-Thiagarajan@users.noreply.github.com>
2023-01-26 13:13:43 -0800

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import numpy as np
 st.title("Intelligent  Papilledema  Detector  (IPD)")
 st.header("By  Priya  Thiagarajan  (21CS007)")
 
-img = st.file_uploader(type="img, jpg")
+img = st.file_uploader(type="jpg")
 
 model = 'new_model.h5'
 #model = load_model(model_name)
