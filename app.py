35a54cd9dd7a5e0df09e0431380fb357adc2a27d
Priya Thiagarajan <88799615+Priya-Thiagarajan@users.noreply.github.com>
2023-01-26 13:01:46 -0800

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,20 @@
 import streamlit as st
+import numpy as np
 
 st.title("Intelligent  Papilledema  Detector  (IPD)")
 st.header("By  Priya  Thiagarajan  (21CS007)")
 
-uploaded_file = st.file_uploader("Choose a File")
+img = st.file_uploader("Choose a File")
+
+model_name = 'new_model.h5'
+model = load(model_name)
+
+pred = model.pred(img)
+x = np.argmax(pred)
+
+if x == 0:
+  st.success("Normal")
+if x == 1:
+  st.warning("Pseudopapilledema")
+if x == 2:
+  st.error("Papilledema")
