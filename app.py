5957f9543991e280d28a167d33dd55314b57ed17
Priya Thiagarajan <88799615+Priya-Thiagarajan@users.noreply.github.com>
2023-01-26 13:03:32 -0800

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import numpy as np
+from tensorflow.keras.models import load_model
 
 st.title("Intelligent  Papilledema  Detector  (IPD)")
 st.header("By  Priya  Thiagarajan  (21CS007)")
@@ -7,7 +8,7 @@ st.header("By  Priya  Thiagarajan  (21CS007)")
 img = st.file_uploader("Choose a File")
 
 model_name = 'new_model.h5'
-model = load(model_name)
+model = load_model(model_name)
 
 pred = model.pred(img)
 x = np.argmax(pred)
