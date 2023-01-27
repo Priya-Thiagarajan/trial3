5caaf3c99b57968c3d00ed35cfb8dc6eac6f53ee
Priya Thiagarajan <88799615+Priya-Thiagarajan@users.noreply.github.com>
2023-01-27 07:52:34 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import numpy as np
 import tensorflow as tf
 from tensorflow.keras.models import load_model
 import tensorflow.keras as keras
+from keras.utils import load_img, img_to_array
 
 st.title("Intelligent  Papilledema  Detector  (IPD)")
 st.header("By  Priya  Thiagarajan  (21CS007)")
@@ -12,6 +13,8 @@ img = st.file_uploader("", type=['png', 'jpg', 'jpeg'])
 model_name = 'new_model.h5'
 model = load_model(model_name)
 
+img = img_to_array(img)
+img = numpy.expand_dims(img, axis=0)
 op = model.predict(img)
 x = np.argmax(op)
 
